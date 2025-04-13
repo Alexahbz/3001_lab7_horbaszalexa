b997479b22c1ec2a6ec46c3ea3928bc4838aee11
Alexahbz <horbaszalexa@gmail.com>
2025-04-12 22:07:48 -0400

    Logic

--- Lab7_Alexandra/Assets/Scripts/Enemy.cs
+++ Lab7_Alexandra/Assets/Scripts/Enemy.cs
@@ -10,22 +10,100 @@ public class Enemy : MonoBehaviour
     public bool hasSniper = false;
     public bool hasShotgun = false;
 
+    private float switchTimer = 0f;
+    private float switchCoolDown = 5f;
+    private enum WeaponType { None, Shotgun, Sniper}
+    private WeaponType currentWeapon = WeaponType.None;
+
     void Update()
     {
-        if (!hasSniper && !hasShotgun)
+        if (!hasSniper || !hasShotgun)
         {
-            FleeFromPlayer();
+            MoveToRemainingWeapon();
+        }
+        if (hasSniper && hasShotgun)
+        {
+            switchTimer += Time.deltaTime;
+            if (switchTimer >= switchCoolDown)
+            {
+                SwitchWeapon();
+                switchTimer = 0f;
+            }
+
+            if (currentWeapon == WeaponType.Sniper)
+            {
+                Debug.Log("Enemy using sniper");
+            }
+            else 
+            {
+                Debug.Log("Enem using shotgun");
+            }
         }
     }
 
-    void FleeFromPlayer()
+    void SwitchWeapon()
     {
-        Vector2 direction = (transform.position - player.position).normalized;
-        Vector2 newPos = (Vector2)transform.position + direction * speed * Time.deltaTime;
+        if (Random.value > 0.5f)
+        {
+            currentWeapon = WeaponType.Sniper;
+        }
+        else
+        {
+            currentWeapon = WeaponType.Shotgun;
+        }
 
-        newPos.x = Mathf.Clamp(newPos.x, -7.5f, 7.5f);
-        newPos.y = Mathf.Clamp(newPos.y, -4f, 4f);
+        Debug.Log("Switched weapon to: " + currentWeapon);
+    }
 
-        transform.position = newPos;
+    void UseSniper()
+    {
+        Debug.Log("Enemy is using the sniper!");
+    }
+    void UseShotgun()
+    {
+        Debug.Log("Enemy is using the shotgun!");
+    }
+
+    void MoveToRemainingWeapon()
+    {
+        GameObject sniper = GameObject.FindWithTag("Sniper");
+        GameObject shotgun = GameObject.FindWithTag("Shotgun");
+
+        GameObject target = null;
+
+        if (!hasSniper && sniper != null)
+        {
+            target = sniper;
+        }
+        else if (!hasShotgun && shotgun != null)
+        {
+            target = shotgun;
+        }
+
+        if (target != null)
+        { 
+            Vector2 direction = (target.transform.position - transform.position).normalized;
+            Vector2 newPos = (Vector2)transform.position + direction * speed * Time.deltaTime;
+
+            newPos.x = Mathf.Clamp(newPos.x, -7.5f, 7.5f);
+            newPos.y = Mathf.Clamp(newPos.y, -4f, 4f);
+            transform.position = newPos;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sniper"))
+        {
+            hasSniper = true;
+            Destroy(other.gameObject);
+            Debug.Log("Enemy picked up a sniper");
+        }
+        else if (other.CompareTag("Shotgun"))
+        {
+            hasShotgun = true;
+            Destroy(other.gameObject);
+            Debug.Log("Enemy picked up a shotgun");
+        }
     }
 }
--- Lab7_Alexandra/Assets/Scripts/Player.cs
+++ Lab7_Alexandra/Assets/Scripts/Player.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float moveSpeed = 3f;
     void Update()
     {
-        
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W) && transform.position.y <= 5.2f)
+        {
+            moveY = moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -moveSpeed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = moveSpeed * Time.deltaTime;
+            
+        }
+        transform.Translate(moveX, moveY, 0f);
     }
 }
